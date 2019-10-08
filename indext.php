06b3e74efde26c330dfe747933e96fc7c4f7afbd
zwdawid <56205879+zwdawid@users.noreply.github.com>
2019-10-08 14:38:05 +0200

    Add files via upload

---
+++ indext.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html lang="pl" >
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+      <form action="mailto:gggg@ggg.pl"method="post"></form>
+        <label>Nazwisko</label><input type="text" name="nazwisko" />
+        <br><br>
+         <label>Imię</label><input type="text" name="imię" />
+          <br><br>
+     <label>password</label><input type="text" name="password" />
+          <br><br>
+  
+    
+
+        <p>Na ile lubisz disco ?</p>
+    <input type="radio" name="disco" value="mniej niż 15">mniej niż 15<br>
+<input type="radio" name="Wiek" value="25-45">15-19<br>
+<input type="radio" name="Wiek" value="50-90">20-29<br>
+<input type="radio" name="Wiek" value="90-100">30-39<br>
+    <p>ile masz lat ?</p>
+<input type="radio" name="Wiek" value="40-60">40-60<br>
+<input type="radio" name="Wiek" value="więcej niż 60 beawo ty">więcej niż 60 
+    
+<input type="submit" value="Wyślij formularz">
+    <form action="mailto:e-mail" method="post" enctype="text/plain"><div>
+<table bgcolor="silver">
+<tr>
+	<td>Imię:</td><td><input type="text" name="imie"></td>
+</tr>
+<tr>
+	<td>Nazwisko:</td><td><input type="text" name="nazwisko"></td>
+</tr>
+<tr>
+	<br>
+	<td colspan="2" align="center"><br>
+		<input type="submit" value="Wyślij">
+		<input type="reset" value="Wyczyść">
+	</td>
+</tr>
+</table>
+</div></form>
+
+
+</body>
+</html>
