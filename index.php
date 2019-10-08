cb9a9ad647bf696d66757f43bca67b4fba5ef9f4
zwdawid <56205879+zwdawid@users.noreply.github.com>
2019-10-08 13:23:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="pl">
+<head>
+	<meta charset="utf8">
+</head>
+<body>	
+	<h1>pierwszy skrypt</h1>
+	
+<?php
+$imie = "Dawid";
+echo "wiatm ".$imie;
+$plyta=400;
+$procesor=1200;
+$pamiec=250;
+ $suma= $plyta+$procesor+$pamiec;
+    echo "Suma:".$suma;
+?>
+
+</body>
+</html>
+
+	
+	
+	
